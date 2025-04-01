b4d4bf4f1192aa1dad96c59eeaaf00b77488765a
kyle1james <lilaresearch@gmail.com>
2025-04-01 11:36:36 -0400

    model

--- utils/stocks.py
+++
@@ -1,7 +0,0 @@
-from dataParser import DataParser
-
-
-class NeuralNet(DataParser):
-    def __init__(self, filepath: str) -> None:
-        # Use super() correctly
-        super().__init__(filepath)
